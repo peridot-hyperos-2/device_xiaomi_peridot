81d31dc1f1219b3405d6284d75ad678075df7a61
kenway214 <kenway214@outlook.com>
2024-12-29 03:51:03 +0530

    peridot: Derpfest-ify
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_peridot.mk
+    $(LOCAL_DIR)/derp_peridot.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_peridot-eng \
-    aosp_peridot-userdebug \
-    aosp_peridot-user
+    derp_peridot-eng \
+    derp_peridot-userdebug \
+    derp_peridot-user
