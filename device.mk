0a9d692ddadcbda9406ce225f1557b498d11c3bb
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2025-05-17 08:43:20 +0000

    peridot: inherit dummy powerhint config

--- device.mk
+++ device.mk
@@ -475,6 +475,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Privapp permissions
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=log
 
